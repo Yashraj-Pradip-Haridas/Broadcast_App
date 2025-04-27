d207020bc0a52153363b172f7070915a6b01e09a
Yashraj <118662533+Dutta159@users.noreply.github.com>
2025-04-27 19:06:41 +0530

    backend setup

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+let allSockets: WebSocket[] = [];
+let userCount = 0;
+
+wss.on("connection", (socket: WebSocket) => {
+  allSockets.push(socket);
+  userCount += 1;
+  console.log("User connected. Number of users:", userCount);
+
+  socket.send("Socket connected successfully");
+
+  socket.on("message", (event: Event) => {
+    const message = event.toString();
+
+    // Send the message to all users except the sender
+    let filteredArray = allSockets.filter((user) => user !== socket);
+
+    filteredArray.forEach((socketElement) => {
+      socketElement.send(message);
+    });
+  });
+
+  socket.on("close", () => {
+    // Remove the socket when user disconnects
+    allSockets = allSockets.filter((user) => user !== socket);
+    userCount -= 1;
+    console.log("User disconnected. Number of users:", userCount);
+  });
+});
